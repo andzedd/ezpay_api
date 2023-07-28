93c1861d01091e9355ad7eb8de8907f6933eed55
Matheus dos Santos Souza <andzedd.matheus@gmail.com>
2023-07-28 20:44:54 -0300

    express routing

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-import express from express
+import express from "express"
+
+const app = express();
+
+const produtos = [
+    {id: 1, "titulo": "Macarrão"},
+    {id: 2, "titulo": "Arroz"},
+    {id: 3, "titulo": "Açúcar"}
+]
+
+app.get('/', (req,res) => {
+    res.status(200).send('EZPAY API')
+})
+
+app.get('/busca', (req,res) => {
+    res.status(200).json(produtos)
+})
+
+export default app
