7073fbf2a6e83f8ba4e39812202d486129335d16
Matheus dos Santos Souza <andzedd.matheus@gmail.com>
2023-07-28 21:05:59 -0300

    added put to products

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express"
 
 const app = express();
 
+app.use(express.json())
+
 const produtos = [
     {id: 1, "titulo": "Macarrão"},
     {id: 2, "titulo": "Arroz"},
@@ -12,8 +14,13 @@ app.get('/', (req,res) => {
     res.status(200).send('EZPAY API')
 })
 
-app.get('/busca', (req,res) => {
+app.get('/produtos', (req,res) => {
     res.status(200).json(produtos)
+})
+
+app.post('/produtos', (req,res) => {
+    produtos.push(req.body)
+    res.status(201).send('Produto cadastrado com sucesso!')
 })
 
 export default app
